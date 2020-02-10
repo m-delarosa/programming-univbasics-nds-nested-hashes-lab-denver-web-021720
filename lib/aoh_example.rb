7dc307df447213105204e505ce24e53d08303c91
Michael De La Rosa <mdelarosa86@gmail.com>
2020-02-10 22:05:00 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -12,7 +12,10 @@ end
 
 def literal_aoh
   array = [
-    {:name => "Don Gately", :occupation =>}]
+    {:name => "Don Gately", :occupation => "Live-in Staff"},
+    {:name => "Joelle van Dyne", :occupation => "Radio Personality" }
+    {:name =>  "Pat Monteseian", :occupation => "Staff" }
+    ]
 end
 
 def aoh_lookup(aoh, row, key)
